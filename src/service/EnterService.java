462d7e959093d4bb209e853b2e14edbfaeaaea2d
shigbbang <shigbbang1234@gmail.com>
2025-07-21 10:08:54 +0900

    걸그룹 작업

---
+++ src/service/EnterService.java
@@ -0,0 +1,14 @@
+package service;
+
+import main.GirlGroupMain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EnterService {
+    private List<Entertainment> entertainments = new ArrayList<>();
+    private List<GirlGroupMain> groups = new ArrayList<>();
+
+    public List<Entertainment> entertainments = new ArrayList<>();
+
+}
---
+++ src/view/ViewProcess.java
@@ -0,0 +1,13 @@
+package view;
+
+public class ViewProcess {
+    public void getEntertainment() {
+        System.out.println("소속사 정보 출력");
+
+
+    }
+
+    public void fideByEnterID() {
+        System.out.println("그룹 정보");
+    }
+}
